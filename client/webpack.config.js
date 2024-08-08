82fd974948dd47b7ee2418e2729e0dd3c45a9983
Yanique Brooks <yaniquebro@gmail.com>
2024-08-08 16:46:25 -0400

    fixed inject manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,8 +23,8 @@ module.exports = () => {
         title: 'Webpack Plugin'
       }),
       new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }), 
       new WebpackPwaManifest({
         fingerprints: false,
